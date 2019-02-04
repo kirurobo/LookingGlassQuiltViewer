965c4ae25294c856f81d1c3cee94737273461af2
Kirurobo <kirurobo@gmail.com>
2019-02-04 23:53:02 +0900

    CPU,GPU利用を抑えた。 [S]キーで保存できるようにした。

--- Assets/Scripts/QuiltFileLoader.cs
+++ Assets/Scripts/QuiltFileLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Kirurobo;
 using HoloPlay;
 
@@ -11,21 +12,109 @@ public class QuiltFileLoader : MonoBehaviour
     Quilt quilt;
     Quilt.Tiling defaultTiling;
 
+    public RenderTexture renderTexture;
+    public GameObject holoplayCapture;
+    public GameObject holoplayUiCamera;
+    public Text messageText;
+
+    bool isHoloPlayCaptureActivated = false;
+
+    float messageClearTime = 0;     // メッセージを表示した場合、それを消去する時刻を入れておく
+
     // Use this for initialization
     void Start()
     {
+        // HoloPlay Captureのオブジェクトを取得
+        if (!holoplayCapture)
+        {
+            holoplayCapture = FindObjectOfType<Capture>().gameObject;
+        }
+
+        if (!holoplayUiCamera)
+        {
+            holoplayUiCamera = FindObjectOfType<ExtendedUICamera>().gameObject;
+        }
+
+        // ファイルドロップなどを扱うためのWindowControllerインスタンスを取得
         window = FindObjectOfType<WindowController>();
         window.OnFilesDropped += Window_OnFilesDropped;
 
+        // Quiltのインスタンスを取得
         quilt = FindObjectOfType<Quilt>();
-        defaultTiling = quilt.tiling;   // Tilingの初期設定
+        defaultTiling = quilt.tiling;   // Tilingの初期設定を記憶しておく
+
+        // バックグラウンド実行を無効にする
+        Application.runInBackground = false;
+
+        // フレームレートを下げる
+        Application.targetFrameRate = 15;
     }
 
     void Update()
     {
-        // [O] キーでファイル選択ダイアログを開く
-        if (Input.GetKey(KeyCode.O)) {
+        // [O] キーまたは右クリックでファイル選択ダイアログを開く
+        if (Input.GetKey(KeyCode.O) || Input.GetMouseButton(1)) {
             OpenFile();
+        }
+
+        // [S] キーで現在の画面を保存
+        if (Input.GetKey(KeyCode.S))
+        {
+            SaveFile();
+        }
+
+        // HoloPlayの処理が有効だったなら、無効にする
+        if (isHoloPlayCaptureActivated)
+        {
+            holoplayCapture.SetActive(false);
+            holoplayUiCamera.SetActive(false);
+
+            isHoloPlayCaptureActivated = false;
+
+            // バックグラウンド実行を無効に戻す
+            Application.runInBackground = false;
+        }
+
+        // メッセージを一定時間後に消去
+        if (messageClearTime > 0)
+        {
+            if (messageClearTime < Time.time)
+            {
+                messageText.text = "";
+                messageClearTime = 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 現在の画面をPNGで保存
+    /// </summary>
+    private void SaveFile()
+    {
+        // 現在のRenderTextureの内容からTexture2Dを作成
+        RenderTexture currentRenderTexture = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+        Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+        texture.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
+        texture.Apply();
+        RenderTexture.active = currentRenderTexture;
+
+        // PNGに変換
+        byte[] rawData = texture.EncodeToPNG();
+        Object.Destroy(texture);
+
+        // 日時を基にファイル名を決定
+        string file = "LookingGlass_" + System.DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".png";
+
+        // 書き出し
+        System.IO.File.WriteAllBytes(file, rawData);
+        Debug.Log("Saved " + file);
+
+        // 保存したというメッセージを表示
+        if (messageText)
+        {
+            messageText.text = "Saved " + file;
+            messageClearTime = Time.time + 5f;      // 5秒後にメッセージ消去とする
         }
     }
 
@@ -47,6 +136,9 @@ public class QuiltFileLoader : MonoBehaviour
     /// <param name="files"></param>
     private void Window_OnFilesDropped(string[] files)
     {
+        // 一時的にバックグラウンド実行を有効にする
+        Application.runInBackground = true;
+
         if (files.Length > 0)
         {
             // 最初のファイルだけ読み込み
@@ -65,11 +157,17 @@ public class QuiltFileLoader : MonoBehaviour
         WWW www = new WWW(file);
         yield return www;
 
+        // レンダリングのため、HoloPlayのオブジェクトを有効化
+        holoplayCapture.SetActive(true);
+        holoplayUiCamera.SetActive(true);
+
+        // Quiltを読み込み
         texture = www.texture;
         quilt.tiling = GetTilingType(texture);
         quilt.overrideQuilt = texture;
         quilt.SetupQuilt();
-        Debug.Log(quilt.tiling.presetName + " # " + quilt.TilingPresetIndex);     // 選択されたTiling
+        isHoloPlayCaptureActivated = true;
+        Debug.Log("Estimaged tiling: " + quilt.tiling.presetName);     // 選択されたTiling
     }
 
     /// <summary>
